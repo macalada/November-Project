4fb8423956b0743ede6c7593dc6f03d577a13aa5
Yanti Sutrisno <wijayanti.sutrisno@live.com>
2020-11-19 12:15:43 -0700

    modify the register form for new member and add rich text editor

--- blog/theblog/models.py
+++ blog/theblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -19,7 +20,8 @@ class Post(models.Model):
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=255, default="coding")
-    body = models.TextField()
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     post_date= models.DateField(auto_now_add=True)
     likes= models.ManyToManyField(User, related_name='blog_post')
 
